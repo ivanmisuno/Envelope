2783fede936ef5efa484f3ebd9c6a84c10d38d7d
Ivan Misuno <im@popappfactory.com>
2018-03-02 00:49:53 +0100

    Add `TwitterAPIServiceRx` example and unit tests to the playground.

--- EnvelopeNetwork.playground/Contents.swift
+++ EnvelopeNetwork.playground/Contents.swift
@@ -4,6 +4,8 @@ import Foundation
 import Alamofire
 import EnvelopeNetwork
 
+// MARK: - Client API
+
 struct TweetsResponse: Decodable {
     // ...
 }
@@ -12,7 +14,7 @@ protocol TwitterAPIServicing {
     func searchTweets(q: String, responseCallback: @escaping (Result<TweetsResponse>) -> ())
 }
 
-class TwitterAPIService: TwitterAPIServicing {
+class TwitterAPIServiceBase {
     struct Configuration {
         let endpointUrl: URL
         static func defaultConfiguration() -> Configuration {
@@ -22,18 +24,22 @@ class TwitterAPIService: TwitterAPIServicing {
         }
     }
 
-    private let network: Networking
-    private let configuration: Configuration
+    fileprivate let network: Networking
+    fileprivate let configuration: Configuration
 
     init(network: Networking,
          configuration: Configuration = Configuration.defaultConfiguration()) {
         self.network = network
         self.configuration = configuration
     }
+}
+
+// MARK: - DataSerializer
+
+class TwitterAPIServiceDataSerializer: TwitterAPIServiceBase, TwitterAPIServicing {
 
     // MARK: - TwitterAPIServicing
 
-    /*
     /// This version of the function uses `DataRequest.dataResponseSerializer()` to get raw response data for illustration purposes only. See the other variant instead.
     func searchTweets(q: String, responseCallback: @escaping (Result<TweetsResponse>) -> ()) {
         network
@@ -54,7 +60,11 @@ class TwitterAPIService: TwitterAPIServicing {
                 }
             })
     }
- */
+}
+
+// MARK: - CodableSerializer
+
+class TwitterAPIServiceCodableSerializer: TwitterAPIServiceBase, TwitterAPIServicing {
 
     func searchTweets(q: String, responseCallback: @escaping (Result<TweetsResponse>) -> ()) {
         network
@@ -65,24 +75,22 @@ class TwitterAPIService: TwitterAPIServicing {
     }
 }
 
-// MARK: - Test spec
-
 import Quick
 import Nimble
 import EnvelopeTest
 
-class TwitterAPIServiceSpec: TestSpec {
+class TwitterAPIServiceCodableSerializerSpec: TestSpec {
     override func spec() {
-        describe("TwitterAPIService") {
+        describe("TwitterAPIServiceCodableSerializer") {
             var network: NetworkingMock!
-            var sut: TwitterAPIService!
+            var sut: TwitterAPIServiceCodableSerializer!
 
             let mockEndpointUrl = URL(string: "https://search.twitter.com/1")!
-            let mockConfiguration = TwitterAPIService.Configuration(endpointUrl: mockEndpointUrl)
+            let mockConfiguration = TwitterAPIServiceCodableSerializer.Configuration(endpointUrl: mockEndpointUrl)
 
             beforeEach {
                 network = NetworkingMock()
-                sut = TwitterAPIService(
+                sut = TwitterAPIServiceCodableSerializer(
                     network: network,
                     configuration: mockConfiguration)
             }
@@ -113,12 +121,87 @@ class TwitterAPIServiceSpec: TestSpec {
                     expect(actualQuery) == mockQuery
                 }
                 it("observed result as expected") {
+                    expect(observedResult).to(beSuccess())
                     expect(observedResult?.value) == mockResult.value
                 }
             } // describe("searchTweets()")
-        } // describe("TwitterAPIService")
+        } // describe("TwitterAPIServiceCodableSerializer")
     }
 }
+
+// MARK: - Rx client
+
+import EnvelopeNetworkRx
+import RxSwift
+import RxTest
+
+protocol TwitterAPIServicingRx {
+    func searchTweets(q: String) -> Single<TweetsResponse>
+}
+
+class TwitterAPIServiceRx: TwitterAPIServiceBase, TwitterAPIServicingRx {
+    // MARK: - TwitterAPIServicingRx
+    func searchTweets(q: String) -> Single<TweetsResponse> {
+        return network
+            .rx.get(configuration.endpointUrl, parameters: ["q": q])
+            .object()
+    }
+}
+
+class TwitterAPIServiceRxSpec: TestSpec {
+    override func spec() {
+        describe("TwitterAPIServiceRx") {
+            var network: NetworkingMock!
+            var sut: TwitterAPIServiceRx!
+
+            let mockEndpointUrl = URL(string: "https://search.twitter.com/1")!
+            let mockConfiguration = TwitterAPIServiceRx.Configuration(endpointUrl: mockEndpointUrl)
+
+            beforeEach {
+                network = NetworkingMock()
+                sut = TwitterAPIServiceRx(
+                    network: network,
+                    configuration: mockConfiguration)
+            }
+
+            describe("searchTweets()") {
+                let mockQuery = "#awesome_testing"
+                let mockResult = Result.success(TweetsResponse())
+                var actualUrl: URL?
+                var actualQuery: String?
+                var observedResult: SingleEvent<TweetsResponse>?
+                beforeEach {
+                    network.mockResponse(mockResult, validateRequest: { (url: URLConvertible, method: HTTPMethod, parameters: Parameters?, encoding: ParameterEncoding, headers: HTTPHeaders?) in
+                        actualUrl = try? url.asURL()
+                        actualQuery = parameters?["q"] as? String
+                    })
+
+                    sut
+                        .searchTweets(q: mockQuery)
+                        .subscribe { (result: SingleEvent<TweetsResponse>) in
+                            observedResult = result
+                        }
+                        .disposed(afterEach: self)
+                }
+                it("network.request() is called") {
+                    expect(network.requestCallCount) == 1
+                }
+                it("correct URL was used") {
+                    expect(actualUrl) == mockEndpointUrl
+                }
+                it("cotrrect parameter was used") {
+                    expect(actualQuery) == mockQuery
+                }
+                it("observed result as expected") {
+                    expect(observedResult).to(beSuccess())
+                    expect(observedResult?.value) == mockResult.value
+                }
+            } // describe("searchTweets()")
+        } // describe("TwitterAPIServiceRx")
+    }
+}
+
+// MARK: - Extensions
 
 extension TweetsResponse: Equatable {
 }
